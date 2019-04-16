c2288818f97cefb98a178bfc883a944cf8f25f68
Natalia Gonzalez <nataliagzzh@gmail.com>
2019-04-16 17:46:09 -0500

    ball now intersects with plants and player

--- src/tutorial1/Nivel3.java
+++ src/tutorial1/Nivel3.java
@@ -26,6 +26,8 @@ public class Nivel3 {
     private LinkedList<PlantLevel3> plants;     //linked list for the small bricks
     private BallLevel3 ball;
     private KeyManager keyManager;
+    private boolean start;          //Boolean that control the start
+    private boolean pause;          //Boolean that control the pause
 
 
 
@@ -36,8 +38,26 @@ public class Nivel3 {
         this.game = game;
         keyManager = new KeyManager();
         plants = new LinkedList<PlantLevel3>();
+        start = false;                  //Se inicializa start en false
+        pause = true;                   //Pause is initialize in true
+    }
+    
+    
+    public boolean isPause() {
+        return pause;
     }
 
+    public void setPause(boolean pause) {
+        this.pause = pause;
+    }
+
+    public boolean isStart() {
+        return start;
+    }
+
+    public void setStart(boolean start) {
+        this.start = start;
+    }
     public int getWidth() {
         return width;
     }
@@ -70,17 +90,52 @@ public class Nivel3 {
         
         //Si se preciona space
         if (getKeyManager().space){
+            setStart(true);
             //Start when space bar is pressed
             ball.setSpeed(2);
         }
 
-        
+                //When p is press
+        if (getKeyManager().pause){
+            //if pause is true
+            if (isPause()){
+                //set pause to false
+                setPause(false);
+                //pStop is called set the key press back to false
+                getKeyManager().kStop();
+            }
+            //if pause is false
+            else{
+                //set pause to true
+                setPause(true);
+                //pStop is called set the key press back to false
+                getKeyManager().kStop();
+            }
+        }
         //Set the plants to their initial positions
             for (int i = 0; i < plants.size(); i++) {
                 PlantLevel3 plant =  plants.get(i);
                 plant.setLives(1);
                 plant.setX(1*(i*70)+ 60);
                 plant.tick();
+                
+                if(ball.intersecta(plant)){
+                //brick lose one life
+                plant.setLives(plant.getLives() - 1);
+                
+                //Make the ball bounce away from brick
+                if(ball.getDirection() == 1)
+                    ball.setDirection(3);
+                
+                else if(ball.getDirection() == 2)
+                    ball.setDirection(4);
+                
+                else if(ball.getDirection() == 3)
+                    ball.setDirection(1);
+                
+                else if(ball.getDirection() == 4)
+                    ball.setDirection(2);
+                }
             }
             
         //Si la pelota intersecta con el player en la mitad derecha
