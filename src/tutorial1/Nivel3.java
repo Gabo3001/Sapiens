ea59952a5a3045b3fac9b3edeed24ba2c3ca011a
Natalia Gonzalez <nataliagzzh@gmail.com>
2019-04-16 17:32:52 -0500

    Added ball, changed sprites for the crops

--- src/tutorial1/Nivel3.java
+++ src/tutorial1/Nivel3.java
@@ -24,6 +24,8 @@ public class Nivel3 {
     String title;
     private PlayerLevel3 player;
     private LinkedList<PlantLevel3> plants;     //linked list for the small bricks
+    private BallLevel3 ball;
+    private KeyManager keyManager;
 
 
 
@@ -32,6 +34,7 @@ public class Nivel3 {
         this.width = width;
         this.height = height;
         this.game = game;
+        keyManager = new KeyManager();
         plants = new LinkedList<PlantLevel3>();
     }
 
@@ -43,23 +46,53 @@ public class Nivel3 {
         return height;
     }
 
+       public KeyManager getKeyManager() {
+        return keyManager;
+    }
+
     public void init() {
         player = new PlayerLevel3(330, getHeight() - 100, 1, 160, 80, game);
+        ball = new BallLevel3(385, getHeight() - 145, 1, 50, 50, game);
+        
+        
         for(int i = 0; i < 5; i++){
-            plants.add(new PlantLevel3(1*(i*60)+ 100, 50, 50, 50, game, 1, 1));
+            plants.add(new PlantLevel3(1*(i*60) + 10, 50, 50, 50, game, 1, 3));
         }
+        
+        
     }
 
     public void tick() {
+         keyManager.tick();
         player.tick();
+        
+         ball.tick();
+        
+        //Si se preciona space
+        if (getKeyManager().space){
+            //Start when space bar is pressed
+            ball.setSpeed(2);
+        }
+
         
         //Set the plants to their initial positions
             for (int i = 0; i < plants.size(); i++) {
                 PlantLevel3 plant =  plants.get(i);
                 plant.setLives(1);
-                plant.setX(1*(i*60)+ 10);
+                plant.setX(1*(i*70)+ 60);
                 plant.tick();
             }
+            
+        //Si la pelota intersecta con el player en la mitad derecha
+        if (player.intersecta(ball)) {
+            //The direction of the ball is changed to 2
+            ball.setDirection(2);
+        }
+        //Si la pelota intersecta con el player en la mitad derecha
+        else if (player.intersecta2(ball)) {
+            //The direction of the ball is changed to 1
+            ball.setDirection(1);
+        }
         
     }
 
@@ -73,6 +106,7 @@ public class Nivel3 {
             
              g.drawImage(Assets.backgroundLevel3, 0, 0, width, height, null);
              player.render(g);
+             ball.render(g);
 
                //render plants
             for (int i = 0; i < plants.size(); i++) {
